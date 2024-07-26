2d5e7aa827a8135cbd97f9080d64f2c9c698b2ca
Jav0proy <166052110+Jav0proy@users.noreply.github.com>
2024-07-26 17:28:56 -0600

    Metodo getElementsByClassName
    Metodo getElementsByClassName

--- indexprueba.php
+++ indexprueba.php
@@ -1,3 +1,37 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+div {
+  border: 1px solid black;
+  padding:8px;
+}
+</style>
+</head>
+<body>
 
-?>
+<h2>Metodo getElementsByClassName()</h2>
+
+<p>
+   
+<div class="ejemplo">
+  <p>Prueba 1</p>
+</div>
+<br>
+<div class="ejemplo">
+  <p>Prueba 2</p>
+</div>
+<br>
+<div class="ejemplocolor">
+  <p>Prueba 3</p>
+</div>
+
+<script>
+const collection = document.getElementsByClassName("ejemplo");
+for(var i =0; i < collection.length; i++){
+collection[i].style.backgroundColor = "red";
+}
+</script>
+
+</body>
+</html>
